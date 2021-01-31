fc2222c05fa32ceb8dbbf26293d1550457c39aa0
shahynaz hanfy <shahynazhanfy@outlook.com>
2021-01-31 14:34:08 +0000

    Initial commit

---
+++ Models/UserRoles.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AssetsApi.Models
+{
+    public class UserRoles
+    {
+         public const string SuperAdmin = "SuperAdmin"; //All Permission
+        public const string Admin = "Admin"; //View Only
+    }
+}
